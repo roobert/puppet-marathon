ede6f7ec9f6e2900e729204a7500bc439815d2e9
Rob Wilson <roobert@gmail.com>
2015-10-12 18:24:26 +0100

    correct service name

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,7 +3,7 @@
 # Ensure marathon is running
 #
 class marathon::service {
-  service { 'marathon::service':
+  service { 'marathon':
     ensure  => running,
     enable  => true,
     require => Class['::marathon::install']
